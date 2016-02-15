22abe7b3f16aabbb75b67f3d6cd56fe1d421d842
Fabian Kirchner <kirchner@posteo.de>
2016-02-15 15:33:10 +0100

    add test script

---
+++ run.sh
@@ -0,0 +1,19 @@
+#! /run/current-system/sw/bin/bash
+
+dist/build/patterns/patterns \
+    --neck_circ=37 \
+    --front_width=32 \
+    --chest_circ=85 \
+    --waist_circ=71 \
+    --hip_height=23 \
+    --hip_circ=89 \
+\
+    --shoulder_width=46 \
+    --back_width=35 \
+    --back_length=44 \
+\
+    --upper_arm_circ=26 \
+    --sleeve_length=60 \
+    --ankle_circ=16 \
+\
+    svgs/triangle.svg
